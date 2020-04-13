e43050be7e1319f4b06531f200da77d3b66661e9
yongwenhao <1012511661@qq.com>
2020-04-13 19:06:08 +0800

    fix:优化

--- src/views/admin/home/components/footer-warp.vue
+++ src/views/admin/home/components/footer-warp.vue
@@ -21,8 +21,7 @@
         </div>
         <div class="footer-cell">
             <p class="title">公司视频</p>
-            <video :src="infoFrom.companyVideoUrl" controls="controls" width="80%"
-                   height="60%"></video>
+            <video src="../../../../assets/conpany.mp4" controls="controls" width="80%" height="100%"></video>
         </div>
     </div>
 </template>
